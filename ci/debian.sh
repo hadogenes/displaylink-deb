73cc1c002035d8bdc9be9924d7937dd9ef4eacb6
Jacek Szafarkiewicz <szafar@linux.pl>
2017-08-13 19:03:08 +0200

    Add libusb dep

--- ci/debian.sh
+++ ci/debian.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
 apt update
-apt install -y build-essential devscripts debhelper wget
+apt install -y build-essential devscripts debhelper \
+    wget \
+    libusb-1.0
 
 cd base-src
 make
